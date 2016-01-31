3af4402c2d37616422336fa7a5250284449e1b6a
songbo <sbo@zju.edu.cn>
2016-02-01 06:09:03 +0800

    fix bug in q2.py

--- lab1/q2.py
+++ lab1/q2.py
@@ -1,9 +1,9 @@
 import math
-n = 127
+n = 47
 l = 16722.0/24/60
 u = 1.0/3
 sum = 0.0
 for x in xrange(0,n+1):
 	sum += (l / u) ** x / math.factorial(x)
-sum *= (l / u) ** n / math.factorial(n) 
-print sum
+res = (l / u) ** n / math.factorial(n) / sum
+print res
