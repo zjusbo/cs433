706593765b4c50a860bad794993ef7c67d84d822
bo song <bo.song@yale.edu>
2016-03-09 21:12:24 -0500

    request sender

--- lab3/src/client/RequestSender.java
+++ lab3/src/client/RequestSender.java
@@ -41,50 +41,49 @@ public class RequestSender implements Runnable {
 			thread.interrupt();
 	}
 
-	public void run(){
+	public void run() {
 		long total_recv_byte_num = 0;
 		long total_recv_packet_num = 0;
 		long total_response_milli_seconds = 0;
 		this.thisThread = Thread.currentThread();
 		Socket socket = null;
-	
-			// while current thread still need to run
-			while(this.thisThread != null){				
-				for(HTTPRequest req : this.requestList){
-					try{
-					if(this.thisThread == null) break; // stop running
+
+		// while current thread still need to run
+		while (this.thisThread != null) {
+			for (HTTPRequest req : this.requestList) {
+				try {
+					if (this.thisThread == null)
+						break; // stop running
 					socket = this.clientSocketFactory.getSocket();
-					//Thread.sleep(1000 * 10);
+					// Thread.sleep(1000 * 10);
 					long recv_byte_num = 0;
 					// write to server
 					print("Sending request: " + req.getURL() + " to " + socket.getInetAddress().getHostName(), 1);
-					DataOutputStream outToServer 
-					   = new DataOutputStream(socket.getOutputStream());
+					DataOutputStream outToServer = new DataOutputStream(socket.getOutputStream());
 					print(req, 2);
 					outToServer.write(req.getBytes());
 					// socket shutdown output
-//					outToServer.flush();
-					//socket.shutdownOutput();
+					// outToServer.flush();
+					// socket.shutdownOutput();
 					long startTime = System.currentTimeMillis();
-					
+
 					// recv response
 					// create read stream and receive from server
 					print("Recieving response from: " + socket.getInetAddress(), 1);
-					BufferedReader inFromServer 
-					 = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+					BufferedReader inFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 					String sentenceFromServer = inFromServer.readLine();
 					long responseTime = System.currentTimeMillis() - startTime;
 					// read recv line by line
 					int content_length = 0;
 					// parse header
-					while(sentenceFromServer != null){
+					while (sentenceFromServer != null) {
 						recv_byte_num += sentenceFromServer.length();
 						String[] tokens = sentenceFromServer.split("\\s+");
-						if(tokens[0].toLowerCase().equals("content-length:")){
+						if (tokens[0].toLowerCase().equals("content-length:")) {
 							content_length = Integer.valueOf(tokens[1]);
 						}
 						print(sentenceFromServer, 2);
-						if(sentenceFromServer.equals("")){ // "\r\n"
+						if (sentenceFromServer.equals("")) { // "\r\n"
 							break; // header is end
 						}
 						sentenceFromServer = inFromServer.readLine();
@@ -92,8 +91,8 @@ public class RequestSender implements Runnable {
 
 					// parse body, is exist
 					int ch;
-					while(content_length > 0){
-						//Debug.DEBUG("Content-length = " + content_length);
+					while (content_length > 0) {
+						// Debug.DEBUG("Content-length = " + content_length);
 						ch = inFromServer.read();
 						recv_byte_num += 1;
 						content_length -= 1;
@@ -104,27 +103,27 @@ public class RequestSender implements Runnable {
 					total_recv_packet_num++;
 					total_response_milli_seconds += responseTime;
 					socket.close();
-					}catch(Exception e){
-						try {
-							socket.close();
-						} catch (IOException e1) {
-							// TODO Auto-generated catch block
-							e1.printStackTrace();
-						}
-						System.err.println("Error message");
-						System.err.println(e.getStackTrace());
-						System.err.println(e.getMessage());
+				} catch (Exception e) {
+					try {
+						socket.close();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					System.err.println("Error message");
+					System.err.println(e.getStackTrace());
+					System.err.println(e.getMessage());
 				}
 			}
 		}
-		
-			// collect data and return
-			synchronized(this.performanceData){
-				this.performanceData.num_bytes += total_recv_byte_num;
-				this.performanceData.num_files += total_recv_packet_num;
-				this.performanceData.response_time += total_response_milli_seconds;
-			}
-		
+
+		// collect data and return
+		synchronized (this.performanceData) {
+			this.performanceData.num_bytes += total_recv_byte_num;
+			this.performanceData.num_files += total_recv_packet_num;
+			this.performanceData.response_time += total_response_milli_seconds;
+		}
+
 	}
 
 	private void print(Object s, int level) {
