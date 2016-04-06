6167afdf505a28e1735c69469ace19b437f0afa4
bo song <bo.song@yale.edu>
2016-04-05 20:53:38 -0400

    stage 1, to debug bind, listen, sendSYN

--- lab4/Fisher/src/proj/TCPManager.java
+++ lab4/Fisher/src/proj/TCPManager.java
@@ -1,6 +1,10 @@
 package proj;
 
+import lib.Callback;
 import lib.Manager;
+import lib.Protocol;
+import lib.Transport;
+import proj.TCPSock.State;
 
 /**
  * <p>Title: CPSC 433/533 Programming Assignment</p>
@@ -18,15 +22,23 @@ public class TCPManager {
     private Node node;
     private int addr;
     private Manager manager;
-
+    private TCPSock[] TCPSocks = new TCPSock[256];
     private static final byte dummy[] = new byte[0];
-
+    
     public TCPManager(Node node, int addr, Manager manager) {
         this.node = node;
         this.addr = addr;
         this.manager = manager;
+        // init tcpsock array
+        for(int i = 0; i < TCPSocks.length; i++){
+        	TCPSocks[i] = new TCPSock(this);
+        }
+        
     }
 
+    public int getAddr(){
+    	return this.addr;
+    }
     /**
      * Start this TCP manager
      */
@@ -45,10 +57,85 @@ public class TCPManager {
      *                 a local port
      */
     public TCPSock socket() {
+    	for(TCPSock sock : TCPSocks){
+    		if(sock.getState() == State.CLOSED){
+    			sock.init();
+    			return sock;
+    		}
+    	}
         return null;
     }
+
+
 
     /*
      * End Socket API
      */
+
+    /*
+     * start internal project API
+     */
+
+    
+    public void addTimer(int deltaT, Callback cb){
+    	this.manager.addTimer(this.addr, deltaT, cb);
+    }
+    /*
+     * send segment using second layer interface 
+     **/
+    public void send(TCPSock sock, Transport segment){
+    	this.node.sendSegment(sock.getLocalAddr(), sock.getRemoteAddr(), Protocol.TRANSPORT_PKT, segment.pack());
+    }
+    
+    /**
+     * Called by node when a TCP packet arrives
+     * demultiplex: decide wich sock should handle this packet based on its 4 tuples
+     *  
+     **/
+	public void onReceive(int srcAddr, int destAddr, Transport segment) {
+		int destPort = segment.getDestPort();
+		int srcPort = segment.getSrcPort();
+		// match connection sock
+		TCPSock sock = getSock(srcAddr, srcPort, destAddr, destPort);
+		// connection sock not found or connection sock is closed
+		if(sock == null || sock.isClosed()){
+			// match welcome sock
+			sock = getSock(destAddr, destPort, TCPSock.State.LISTEN);
+		}
+		if(sock != null){
+			sock.onReceive(srcAddr, srcPort, segment);
+		}else{
+			String msg = String.format("No matching sock on server: target sock is (%d, %d, %d, %d)", srcAddr, srcPort, destAddr, destPort);
+			Debug.print(msg);
+		}
+	}
+	public TCPSock getSock(int destAddr, int destPort, State state) {
+		for(TCPSock sock: TCPSocks){
+			if(sock.getLocalAddr() == destAddr && sock.getLocalPort() == destPort && sock.getState() == state){
+				return sock;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * get sock from its 4 tuples
+	 **/
+	public TCPSock getSock(int srcAddr, int srcPort, int destAddr, int destPort){
+		for(TCPSock sock: TCPSocks){
+			if(sock.getRemoteAddr() == srcAddr && sock.getRemotePort() == srcPort && sock.getLocalAddr() == destAddr && sock.getLocalPort() == destPort){
+				return sock;
+			}
+		}
+		return null;
+	}
+	
+	public TCPSock getSock(int destAddr, int destPort){
+		for(TCPSock sock: TCPSocks){
+			if(sock.getLocalAddr() == destAddr && sock.getLocalPort() == destPort){
+				return sock;
+			}
+		}
+		return null;
+	}
 }
--- lab4/Fisher/src/proj/TCPSock.java
+++ lab4/Fisher/src/proj/TCPSock.java
@@ -1,4 +1,14 @@
 package proj;
+
+import java.lang.reflect.Method;
+import java.nio.ByteBuffer;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+import lib.Callback;
+import lib.Transport;
+
 /**
  * <p>Title: CPSC 433/533 Programming Assignment</p>
  *
@@ -16,17 +26,52 @@ public class TCPSock {
     // TCP socket states
     enum State {
         // protocol states
-        CLOSED,
+        CLOSED, // closed, not used, ready to be reused
+        INIT, // newly created
         LISTEN,
         SYN_SENT,
         ESTABLISHED,
         SHUTDOWN // close requested, FIN not sent (due to unsent data in queue)
     }
-    private State state;
-
-    public TCPSock() {
+    private State state = State.CLOSED; // init sock state as closed
+    private TCPManager tcpMan;
+	private int localAddr = -1;
+    private int localPort = -1;
+    private int remoteAddr = -1;
+    private int remotePort = -1;
+    
+    private int send_seq_num, send_wnd_size, send_wnd_base, send_wnd_limit, send_wnd_next;
+    
+    
+    private int backlog = 0;
+    private ByteBuffer recv_buf, send_buf;
+    
+    private List<TCPSock> pendingConnections = null;
+    public TCPSock(TCPManager manager) {
+    	this.tcpMan = manager;
+    	
     }
 
+    public void init(){
+    	localPort = remoteAddr = remotePort = -1;
+    	this.localAddr = this.tcpMan.getAddr();
+    	this.state = State.INIT;
+    	recv_buf = ByteBuffer.allocate(4096); // one page
+    	send_buf = ByteBuffer.allocate(4096); // one page
+    }
+    
+    public void clean(){
+    	localAddr = localPort = remoteAddr = remotePort = -1;
+    	state = State.CLOSED;
+    	recv_buf = null;
+    	send_buf = null;
+    	for(TCPSock sock : pendingConnections){
+    		sock.clean();
+    		
+    	}
+    	pendingConnections = null;
+    }
+    
     /*
      * The following are the socket APIs of TCP transport service.
      * All APIs are NON-BLOCKING.
@@ -38,8 +83,15 @@ public class TCPSock {
      * @param localPort int local port number to bind the socket to
      * @return int 0 on success, -1 otherwise
      */
+    
     public int bind(int localPort) {
-        return -1;
+    	// sock is initialized and localPort is not occupied.
+    	if(this.state == State.INIT && this.tcpMan.getSock(this.localAddr, localPort) == null){
+        	Debug.println(String.format("%s: Binded to port %d", this.toString(), this.localPort));
+    		this.localPort = localPort;
+    		return 0;
+    	}
+    	return -1;
     }
 
     /**
@@ -48,16 +100,37 @@ public class TCPSock {
      * @return int 0 on success, -1 otherwise
      */
     public int listen(int backlog) {
-        return -1;
+    	if(this.localPort != -1 && this.state == State.INIT){
+        	Debug.println(String.format("%s: Listening at port %d", this.toString(), this.localPort));
+    		this.pendingConnections = new LinkedList<TCPSock>();
+    		this.backlog = backlog;
+    		this.state = State.LISTEN;
+    		return 0;
+    	}
+    	return -1;
+ 
     }
 
-    /**
+    public State getState() {
+		return state;
+	}
+
+	public void setState(State state) {
+		this.state = state;
+	}
+
+	/**
      * Accept a connection on a socket
      *
      * @return TCPSock The first established connection on the request queue
      */
     public TCPSock accept() {
-        return null;
+        if(this.state == State.LISTEN && !pendingConnections.isEmpty()){
+        	Debug.println(String.format("%s: Accepting", this.toString()));
+        	TCPSock sock = this.pendingConnections.remove(0);
+        	return sock;
+        }
+    	return null;
     }
 
     public boolean isConnectionPending() {
@@ -78,19 +151,47 @@ public class TCPSock {
 
     /**
      * Initiate connection to a remote socket
-     *
      * @param destAddr int Destination node address
      * @param destPort int Destination port
      * @return int 0 on success, -1 otherwise
      */
     public int connect(int destAddr, int destPort) {
+    	Debug.println(String.format("Socket (%d:%d) connecting to (%d:%d)", this.localAddr, this.localPort, this.remoteAddr, this.remotePort));
+    	if(this.state == State.INIT && this.localPort != -1){
+    		this.remoteAddr = destAddr;
+    		this.remotePort = destPort;
+    		this.state = State.SYN_SENT;
+    		Random rand = new Random();
+    		this.send_seq_num = rand.nextInt(1024);
+    		sendSYN();
+    		return 0;
+    	}
         return -1;
     }
 
-    /**
+    private void sendSYN() {
+    	Debug.println("In function sendSYN");
+    	if(this.state == State.SYN_SENT){
+    		Transport segment = new Transport(this.localPort, this.remotePort, Transport.SYN, 1, this.send_seq_num, null);
+    		this.tcpMan.send(this, segment);
+    		try{
+    			Method method = Callback.getMethod("sendSYN", this, null);
+                Callback cb = new Callback(method, (Object)this, null);
+                this.tcpMan.addTimer(1000, cb);
+    		}catch(Exception e){
+    			Debug.println("Failed to add timer. sendSYN");
+    		}
+    	}else{
+    		Debug.print("trying to send SYN in a wrong socket state\n");
+    	}
+    	
+	}
+
+	/**
      * Initiate closure of a connection (graceful shutdown)
      */
     public void close() {
+    	
     }
 
     /**
@@ -130,4 +231,89 @@ public class TCPSock {
     /*
      * End of socket API
      */
+    
+    public int getLocalAddr() {
+		return localAddr;
+	}
+
+	public void setLocalAddr(int localAddr) {
+		this.localAddr = localAddr;
+	}
+
+	public int getLocalPort() {
+		return localPort;
+	}
+
+	public void setLocalPort(int localPort) {
+		this.localPort = localPort;
+	}
+
+	public int getRemoteAddr() {
+		return remoteAddr;
+	}
+
+	public void setRemoteAddr(int remoteAddr) {
+		this.remoteAddr = remoteAddr;
+	}
+
+	public int getRemotePort() {
+		return remotePort;
+	}
+
+	public void setRemotePort(int remotePort) {
+		this.remotePort = remotePort;
+	}
+
+	/**
+	 * Called by TCPmanager if a packet arrives at node and match this sock
+	 **/
+	public void onReceive(int srcAddr, int srcPort, Transport segment) {
+		switch(segment.getType()){
+			case Transport.SYN:
+				receiveSYN(srcAddr, srcPort, segment);
+				break;
+			case Transport.DATA:
+				receiveData(srcAddr, srcPort, segment);
+				break;
+			case Transport.ACK:
+				receiveACK(srcAddr, srcPort, segment);
+				break;
+			case Transport.FIN:
+				receiveFIN(srcAddr, srcPort, segment);
+				break;
+			default:
+				Debug.print(String.format("Unknown TCP package type %s", segment.toString()));
+				break;
+		}
+	}
+
+	private void receiveFIN(int srcAddr, int srcPort, Transport segment) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void receiveACK(int srcAddr, int srcPort, Transport segment) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void receiveData(int srcAddr, int srcPort, Transport segment) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void receiveSYN(int srcAddr, int srcPort, Transport segment) {
+		System.out.print("S");
+		if(this.getState() == State.LISTEN){
+			// create a new connection socket
+			
+		}//else if(this.getState() == State.ESTABLISHED && ){
+			
+		//}
+	}
+
+	@Override
+	public String toString(){
+		return String.format("Socket (%d:%d , %d:%d)", this.localAddr, this.localPort, this.remoteAddr, this.remotePort);
+	}
 }
